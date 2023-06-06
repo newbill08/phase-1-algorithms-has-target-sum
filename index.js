1c8215d142fff97c0618927d666ed6dd2b3bc048
joshua <josh.newbill08@hotmail.com>
2023-06-06 13:36:38 -0500

    target reached

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  /*
+Write a function called hasTargetSum that receives two arguments:
+an array of integers
+a target integer
+The function should return true if any pair of numbers in the array adds up to the target number.
+
+  Take in array and target
+  Iterate through i for loop
+  Iterate through j for loop
+  check if i+j === target
+  return true  
+
+*/
+
+for(let i=0; i< array.length; i++){
+ // console.log(array[i])
+  for (let j=i+1; j< array.length; j++){
+   // console.log(array[j])
+    if(array[i]+array[j] === target){
+      return true
+    } 
+  }
+}
+return false
 }
 
 /* 
